f4c8826d81f63775e961f533fa18c47219740bca
vanGamo <vangamo.beta@gmail.com>
2024-08-29 12:56:57 +0200

    Add MDXEditor as editor.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Blog web editor
 
 This web app is based on Vite React template (`npx create-vite blog-editor --template react`).
+
+Is using [MDXEditor](https://mdxeditor.dev/editor/docs/getting-started) (`pnpm i @mdxeditor/editor`) to edit and pre-visualize MarkDown.
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,22 @@
+import {
+  MDXEditor,
+  headingsPlugin,
+  markdownShortcutPlugin,
+} from "@mdxeditor/editor";
+
 function Editor() {
+  const handleChange = (...params) => {
+    console.log(params);
+  };
+
   return (
-    <textarea className="editor tablet:-translate-y-20">Markdown content</textarea>
+    <MDXEditor
+      className="editor tablet:-translate-y-20"
+      /* ref={ref} */
+      markdown="Markdown content"
+      onChange={handleChange}
+      plugins={[headingsPlugin(), markdownShortcutPlugin()]}
+    />
   );
 }
 
