5f4c9e447b8dc2d0601ce1f6113c0cb620eedad2
vanGamo <vangamo.beta@gmail.com>
2024-08-29 13:46:25 +0200

    Add more options as plugins to editor.

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,8 +1,27 @@
 import {
   MDXEditor,
   headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  linkPlugin,
+  UndoRedo,
+  BlockTypeSelect,
+  BoldItalicUnderlineToggles,
+  StrikeThroughSupSubToggles,
+  CodeToggle,
+  InsertCodeBlock,
+  CreateLink,
+  toolbarPlugin,
   markdownShortcutPlugin,
+  linkDialogPlugin,
+  ListsToggle,
 } from "@mdxeditor/editor";
+
+import "@mdxeditor/editor/style.css";
+import "./Editor.css";
 
 function Editor({content, onChange}) {
   const handleChange = (content) => {
@@ -17,8 +36,40 @@ function Editor({content, onChange}) {
       /* ref={ref} */
       markdown={content}
       onChange={handleChange}
-      plugins={[headingsPlugin(), markdownShortcutPlugin()]}
-    />
+      plugins={[
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              {" "}
+              <UndoRedo />
+              <BlockTypeSelect />
+              <BoldItalicUnderlineToggles />
+              <StrikeThroughSupSubToggles />
+              <ListsToggle />
+              <CodeToggle />
+              <InsertCodeBlock />
+              <CreateLink />
+            </>
+          ),
+        }),
+        headingsPlugin(),
+        quotePlugin(),
+        listsPlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+        linkPlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "js" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            js: "JavaScript",
+            jsx: "React",
+            css: "CSS",
+            bash: "BASH command",
+          },
+        }),
+        linkDialogPlugin(),
+      ]}
+    />    
   );
 }
 
