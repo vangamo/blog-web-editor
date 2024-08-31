c3733aaee79c7d2e9a17d0a77d1486693d2fc35f
vanGamo <vangamo.beta@gmail.com>
2024-08-31 11:38:58 +0200

    Implement animation to fade out on close toast.

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,32 +1,41 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function Toast({ isShown = true, title, onClose }) {
   if (!isShown) {
     return null;
   }
 
+  const [classAnimation, setClassAnimation] = useState("");
   let timeout = null;
 
   useEffect(() => {
     timeout = setTimeout(() => {
-      onClose();
-    }, 1500);
+      setClassAnimation("fade-out");
+    }, 4000);
   }, []);
 
   const handleClose = () => {
     clearTimeout(timeout);
 
+    setClassAnimation("fade-out");
+  };
+
+  const handleAnimationEnd = () => {
     onClose();
   };
 
   return (
     <div
-      class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 fixed bottom-5 right-5 z-10"
+      className={
+        classAnimation +
+        " flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 fixed bottom-5 right-5 z-10"
+      }
       role="alert"
+      onAnimationEnd={handleAnimationEnd}
     >
-      <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+      <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
         <svg
-          class="w-5 h-5"
+          className="w-5 h-5"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -34,19 +43,19 @@ function Toast({ isShown = true, title, onClose }) {
         >
           <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
         </svg>
-        <span class="sr-only">Check icon</span>
+        <span className="sr-only">Check icon</span>
       </div>
-      <div class="ms-3 text-sm font-normal">{title}</div>
+      <div className="ms-3 text-sm font-normal">{title}</div>
       <button
         type="button"
-        class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+        className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
         data-dismiss-target="#toast-success"
         aria-label="Close"
         onClick={handleClose}
       >
-        <span class="sr-only">Close</span>
+        <span className="sr-only">Close</span>
         <svg
-          class="w-3 h-3"
+          className="w-3 h-3"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
